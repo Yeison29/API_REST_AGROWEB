e936790d9b616884d3206ac2fb21dee04988806c
Yeison29 <106707909+Yeison29@users.noreply.github.com>
2024-08-23 16:30:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from src.aplications.app import ap
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(ap.app, host="127.0.0.1", port=8000)
+    uvicorn.run(ap.app, host="0.0.0.0", port=8080)
